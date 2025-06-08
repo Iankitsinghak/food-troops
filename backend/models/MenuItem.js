384bfb724b681649b169fa986dadfc1ff1357aa0
Ankit Singh <ankitsinghak9051@gmail.com>
2025-06-08 22:11:52 +0530

    Update MenuItem.js

--- backend/models/MenuItem.js
+++ backend/models/MenuItem.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const MenuItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: String,
+  category: { type: String, required: true },
   price: { type: Number, required: true },
   ingredients: [String],
-  tags: [String], // e.g. vegan, spicy
-  available: { type: Boolean, default: true }
-}, { timestamps: true });
+  tags: [String],
+  available: { type: Boolean, default: true },
+});
 
 module.exports = mongoose.model("MenuItem", MenuItemSchema);
