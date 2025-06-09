d4abc51ccef4123db97e83d246a06f6bc54f9eb3
Ankit Singh <ankitsinghak9051@gmail.com>
2025-06-09 14:30:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,11 @@
 // Vite config here
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+})
