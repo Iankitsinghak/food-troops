8f87b4ac1b8751f66b46721ce9a3907d9e836414
Ankit Singh <ankitsinghak9051@gmail.com>
2025-06-09 15:20:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/orders', order);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB error:', err));
