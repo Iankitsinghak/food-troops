40e48d707818f5fc9fdee4798de34a723771f40a
Ankit Singh <ankitsinghak9051@gmail.com>
2025-06-09 15:12:44 +0530

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,15 +1,14 @@
-
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const OrderSchema = new mongoose.Schema({
-    items: [{
-        itemId: String,
-        name: String,
-        quantity: Number,
-        notes: String
-    }],
-    status: { type: String, default: 'Placed' },
-    timestamp: { type: Date, default: Date.now }
+  items: [{
+    itemId: String,
+    name: String,
+    quantity: Number,
+    notes: String
+  }],
+  status: { type: String, default: 'Placed' },
+  timestamp: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Order', OrderSchema);
+export default mongoose.model('Order', OrderSchema);
